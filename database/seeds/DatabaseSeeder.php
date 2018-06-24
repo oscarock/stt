0ac090d1aebe4a95d645467c7041b83cd3dcb13a
Oscar Diaz <o-pipe@hotmail.com>
2018-06-24 01:27:30 -0500

    Crea un seed de prueba

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+         DB::table('users')->insert([
+            'name' => 'Oscar',
+            'last_name' => 'Diaz',
+            'civil_status' => 'single',
+            'birth_date' => '1991-01-17',
+            'salary' => 500000,
+            'email' => 'prueba@prueba.com',
+            'department' => 'Bogota D.C',
+            'town' => 'Bogota D.C',
+            'password' => Hash::make(123456);
+        ]);
     }
 }
